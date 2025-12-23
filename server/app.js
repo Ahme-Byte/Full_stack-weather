44317cf20a144385a58d8d478b008d31b26dfb31
Muhammad Ahmad <ahmipersonal05@gmail.com>
2025-12-23 17:56:36 +0500

    test10

--- server/app.js
+++ server/app.js
@@ -35,38 +35,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-/* ---------- DATABASE CONNECTION ---------- 
+/* ---------- DATABASE CONNECTION ---------- */
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.qpst8rv.mongodb.net/weatherdb?retryWrites=true&w=majority`)
   .then(() => console.log('Database connected'))
-  .catch(err => console.error('MongoDB Error:', err.message));*/
-
-
-
-
-// ---------- DATABASE CONNECTION ----------
-const sanitizeEnv = (val = "") => val.replace(/^"(.*)"$/, "$1").trim();
-
-// Hardcode username if env is unreliable
-const mongoUser = "ahmipersonal05_db_user"; 
-const mongoPass = sanitizeEnv(process.env.MONGODB_PASS || "");
-
-if (!mongoPass) {
-  console.error("MongoDB password is empty! Check Railway env variable MONGODB_PASS");
-  process.exit(1); // stop server to avoid empty auth
-}
-
-const encodedPass = encodeURIComponent(mongoPass);
-const mongoHost = "cluster0.qpst8rv.mongodb.net";
-const dbName = "weatherdb";
-
-console.log(`Connecting to MongoDB as user ${mongoUser}...`);
-
-mongoose.connect(
-  `mongodb+srv://${mongoUser}:${encodedPass}@${mongoHost}/${dbName}?retryWrites=true&w=majority`
-)
-.then(() => console.log("Database connected"))
-.catch(err => console.error("MongoDB Error:", err.message));
-
+  .catch(err => console.error('MongoDB Error:', err.message));
 
 /* ---------- PORT LISTENING ---------- */
 const PORT = process.env.PORT || 8080;
